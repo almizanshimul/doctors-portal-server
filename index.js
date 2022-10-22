177421b93e5941bc40a7f0c822c0f96adf17eaab
Al <almizan1040@gmail.com>
2022-10-22 09:45:33 +0600

    require admin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const app = express()
@@ -13,7 +14,20 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eruzs6g.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.JWT_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 async function run() {
     try {
@@ -30,6 +44,26 @@ async function run() {
             res.send(services)
         })
 
+
+        app.get('/users', verifyJWT, async (req, res) => {
+            const cursor = await usersCollection.find({}).toArray()
+            res.send(cursor)
+        })
+
+
+        // admin verify process 
+        app.get('/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email
+            const user = await usersCollection.findOne({ email: email })
+            const isAdmin = user.role === 'admin'
+            res.send({ admin: isAdmin })
+
+        })
+        /**  
+         * create token create step
+         * step 1: open terminal and enter node
+         * step 2: require('crypto').randomBytes(64).toString('hex')                         
+        */
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email
             const user = req.body
@@ -39,7 +73,28 @@ async function run() {
                 $set: user,
             }
             const result = await usersCollection.updateOne(filter, updateDoc, options)
-            res.send(result)
+            const token = jwt.sign({ email: email }, process.env.JWT_TOKEN, { expiresIn: '1h' });
+            res.send({ result, token })
+        })
+
+
+        // Admin user 
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email
+            const requester = req.decoded.email
+            const requesterAccount = await usersCollection.findOne({ email: requester })
+            if (requesterAccount.role == 'admin') {
+                const filter = { email: email }
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                }
+                const result = await usersCollection.updateOne(filter, updateDoc)
+                res.send(result)
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' })
+            }
+
         })
 
 
@@ -74,12 +129,18 @@ async function run() {
          * app.delete('/booking/:id')
          */
 
-        app.get('/booking', async (req, res) => {
+        app.get('/booking', verifyJWT, async (req, res) => {
             const patientEmail = req.query.email;
-            const query = { email: patientEmail }
-            // console.log(query);
-            const booking = await bookingCollection.find(query).toArray()
-            res.send(booking)
+            const decodedEmail = req.decoded.email;
+            if (patientEmail == decodedEmail) {
+                const query = { email: patientEmail }
+                // console.log(query);
+                const booking = await bookingCollection.find(query).toArray()
+                return res.send(booking)
+            } else {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
+
         })
         app.post('/booking', async (req, res) => {
             const booking = req.body;
